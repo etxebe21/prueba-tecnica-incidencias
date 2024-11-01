4c58c452b6310dfd90e3de7534e4572a0e2401a6
Etxebe <mikelgartzia21@gmail.com>
2024-11-01 18:05:54 +0100

    [NEW] configurado tailwind, alpine y livewire

---
+++ incident-manager/tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: [
+    './storage/framework/views/*.php',
+    './resources/views/**/*.blade.php',
+    './resources/js/**/*.js',
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
